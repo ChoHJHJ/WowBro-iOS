846fba961123ea485d67a9968e97b6fd2b90bb97
조현중 <chj113024@gmail.com>
2022-03-18 21:08:42 +0900

    사이드 메뉴 서버 연동, 회원 수정, 탈퇴 기능 구현

--- WowBro/Controller/SideMenuViewController.swift
+++ WowBro/Controller/SideMenuViewController.swift
@@ -6,13 +6,43 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class SideMenuViewController: UIViewController {
     @IBOutlet weak var photo: UIImageView!
+    @IBOutlet var userIdLabel: UILabel!
+    @IBOutlet var nickNameLabel: UILabel!
     
+    var currentId: String?
     
     override func viewDidLoad() {
         photo.layer.cornerRadius = 30
+        
+        let preferences = UserDefaults.standard
+
+        if preferences.object(forKey: "insertedId") == nil {
+            print("Doesn't exist!")
+        } else {
+            currentId = preferences.string(forKey: "insertedId")
+        }
+        
+        let urlString = "http://localhost:3000/userInfo/\(currentId!)"
+        let encodedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let url = URL(string: encodedString)!
+        let alamo = AF.request(url, method: .get, encoding: JSONEncoding.default)
+        alamo.validate().responseJSON { response in
+            switch response.result {
+            case .success(_):
+                if let json = try? JSON(data: response.data!) {
+                    let result = json
+                    self.userIdLabel.text = result[0]["userID"].stringValue
+                    self.nickNameLabel.text = result[0]["userNickName"].stringValue + "님"
+                }
+            case .failure(let error):
+                debugPrint(error)
+            }
+        }
     }
     
     @IBAction func dismiss(_ sender: UIButton) {
--- WowBro/Controller/UpdateViewController.swift
+++ WowBro/Controller/UpdateViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class UpdateViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
@@ -14,10 +15,20 @@ class UpdateViewController: UIViewController {
     @IBOutlet weak var updateButton: UIButton!
     @IBOutlet weak var withdrawButton: UIButton!
     
+    var currentId: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         initView()
+        
+        let preferences = UserDefaults.standard
+
+        if preferences.object(forKey: "insertedId") == nil {
+            print("Doesn't exist!")
+        } else {
+            currentId = preferences.string(forKey: "insertedId")
+        }
     }
     
     func initView() {
@@ -31,7 +42,54 @@ class UpdateViewController: UIViewController {
         nickNameTextField.textFieldBorder()
     }
     
+    @IBAction func btnUpdate(_ sender: UIButton) {
+        if let userPassword = passwordTextField.text, let userPassword2 = password2TextField.text, let nickName = nickNameTextField.text {
+            if userPassword != userPassword2 {
+                let alert = UIAlertController(title: "비밀번호 확인", message: "일치하는 비밀번호를 입력해주세요", preferredStyle: UIAlertController.Style.alert)
+                let action = UIAlertAction(title: "OK", style: .default)
+                alert.addAction(action)
+                present(alert, animated: false, completion: nil)
+            } else if (userPassword == "" || userPassword2 == "" || nickName == "") {
+                let alert = UIAlertController(title: "입력 오류", message: "모든 항목을 입력해주세요", preferredStyle: UIAlertController.Style.alert)
+                let action = UIAlertAction(title: "OK", style: .default)
+                alert.addAction(action)
+                present(alert, animated: true, completion: nil)
+            } else {
+                let url = "http://localhost:3000/userInfo/\(currentId!)"
+                let param: Parameters = [
+                    "userPassword": passwordTextField.text!,
+                    "userNickName": nickNameTextField.text!]
+                let alamo = AF.request(url, method: .patch,
+                            parameters: param,
+                            encoding: URLEncoding.httpBody)
+                
+                alamo.responseJSON() {
+                    response in
+                    print("전송 성공")
+                }
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+    
     @IBAction func btnWithdraw(_ sender: UIButton) {
-        self.navigationController?.popToRootViewController(animated: true)
+        UserDefaults.standard.removeObject(forKey: "insertedId")
+        let alert = UIAlertController(title: "회원탈퇴", message: "탈퇴하시겠습니까?", preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "예", style: .destructive) { action in
+            self.dismiss(animated: true) {
+                let url = "http://localhost:3000/userInfo/\(self.currentId!)"
+                let alamo = AF.request(url, method: .delete,
+                            encoding: URLEncoding.httpBody)
+                alamo.responseJSON() {
+                    response in
+                    print("전송 성공")
+                }
+                self.navigationController?.popToRootViewController(animated: true)
+            }
+        }
+        let cancel = UIAlertAction(title: "아니오", style: .cancel, handler : nil)
+        alert.addAction(okAction)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
     }
 }
